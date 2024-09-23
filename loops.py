dc232e9b82b208d605f411bb22b2ab87e739ad38
Henshaw <162965939+alexshaw13@users.noreply.github.com>
2024-09-23 13:02:43 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-value= ["is zero","is positive","is negative"]
 
-for number in range(-10, 10):
-    if number==0:
-        print(number,value[0])
-      
-    elif number>0:
-        print(number,value[1])
-        
-    elif number<0:
-        print(number,value[2])
+if number == 0:
+    print(f"{number} is zero")
+    
+elif number > 0:
+    print(f"{number} is positive")
+    
+elif number < 0:
+    print(f"{number} is negative")
 
 
-        
